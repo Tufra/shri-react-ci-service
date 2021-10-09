d2d0f48653f011965b54cdd9cc6245c814a270c0
Tufra <60471036+Tufra@users.noreply.github.com>
2021-10-09 16:47:42 +0300

    settings validation

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -10,30 +10,45 @@ import {TimeInput} from "./form/TimeInput";
 
 export function Settings() {
     const settings = React.useContext(settingsContext)
+    const [status, setStatus] = React.useState("")
     const history = useHistory()
 
     console.log(settings.settings)
 
-    function saveSettings(event) {
+    async function saveSettings(event) {
         event.preventDefault()
-        const form = event.target
+        setStatus("Cloning...")
 
+        const form = event.target
         const repoName = form[0].value
         const buildCommand = form[1].value
         const branch = form[2].value
         const time = form[3].value
 
-        const obj = {
-            repoName: repoName,
-            buildCommand: buildCommand,
-            branch: branch,
-            time: time
-        }
-        console.log(obj)
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const isValid = /[\._\-a-z0-9A-Z]*\/[\._\-a-z0-9A-Z]*/.test(repoName)
+                if (isValid) resolve()
+                else reject()
+            }, 2000)
+        }).then(
+            () => {
+                const obj = {
+                    repoName: repoName,
+                    buildCommand: buildCommand,
+                    branch: branch,
+                    time: time
+                }
+                console.log(obj)
 
-        settings.setSettings(obj)
+                settings.setSettings(obj)
 
-        history.push('/')
+                history.push('/')
+            },
+            () => {
+                setStatus('GitHub repository name error')
+            }
+        )
         //console.log(settings)
     }
 
@@ -52,15 +67,32 @@ export function Settings() {
                 <p className={'settings-header'}>Settings</p>
                 <p className={'settings-desc'}>Configure repository connection and synchronization settings.</p>
                 <form action="" onSubmit={saveSettings}>
-                    <Input inputName={'GitHub repository'} value={settings.settings?.repoName} required placeholder={'user-name/repo-name'}/>
-                    <Input inputName={'Build command'} value={settings.settings?.buildCommand} required placeholder={'npm ci && npm run build'}/>
-                    <Input inputName={'Main branch'} value={settings.settings?.branch} placeholder={'master |'}/>
-                    <TimeInput inputName={'Time'} value={settings.settings?.time}/>
+                    <Input inputName={'GitHub repository'}
+                           value={settings.settings?.repoName}
+                           required placeholder={'user-name/repo-name'}
+                    />
+                    <Input inputName={'Build command'}
+                           value={settings.settings?.buildCommand}
+                           required placeholder={'npm ci && npm run build'}
+                    />
+                    <Input inputName={'Main branch'}
+                           value={settings.settings?.branch}
+                           placeholder={'master |'}
+                    />
+                    <TimeInput inputName={'Time'}
+                               value={settings.settings?.time}
+                    />
+                    <p className={"settings-status"}>{status}</p>
                     <div className={"settings-button-cont"}>
-                        <input type="submit" className={"global-action-button"} value={'Save'}/>
+                        <input type="submit"
+                               className={"global-action-button"}
+                               value={'Save'}
+                               disabled={status === 'Cloning...'}
+                        />
                         <input type="button"
                                className={"global-control-button"}
                                value={'Cancel'}
+                               disabled={status === 'Cloning...'}
                                onClick={goBack}
                         />
                     </div>
