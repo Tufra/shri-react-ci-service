5c36cd9c442f2f01cff5ed6d20af250bcfce69d8
Tufra <60471036+Tufra@users.noreply.github.com>
2021-11-05 14:28:42 +0300

    Update release.sh

--- .github/release.sh
+++ .github/release.sh
@@ -14,9 +14,9 @@ echo "$COMMITS"
 #     "https://api.tracker.yandex.net/v2/myself"
 #
 #echo aaa
-#curl -H "Content-Type: application/json" \
-#     -H "Authorization: OAuth AQAAAAAc1G31AAd4vp4Ts7KVD0dTnFyQ5N3VliU" \
-#     -H "X-Org-ID: 6461097" \
-#     https://api.tracker.yandex.net/v2/issues/ \
-#     -d "{\"queue\": \"TMP\",\"summary\": \"Release $TAG\",\"description\": \"$COMMITS\"}" \
-#     > ticket.txt
+curl -H "Content-Type: application/json" \
+     -H "Authorization: OAuth AQAAAAAc1G31AAd4vp4Ts7KVD0dTnFyQ5N3VliU" \
+     -H "X-Org-ID: 6461097" \
+     https://api.tracker.yandex.net/v2/issues/ \
+     -d "{\"queue\": \"TMP\",\"summary\": \"Release $TAG\",\"description\": \"$COMMITS\"}" \
+     > ticket.txt
