49a0bb7ae69b82bfce73cb48d3ebc3ca5160f61c
Tufra <60471036+Tufra@users.noreply.github.com>
2021-11-05 14:20:57 +0300

    commit

--- .github/createImage.sh
+++ .github/createImage.sh
@@ -10,3 +10,5 @@ docker run -p 49160:8080 -d tufra/ci-service-"$TAG"
 
 docker exec "tufra/ci-service-$TAG" <putContainerInfo.sh
 docker exec "tufra/ci-service-$TAG" <test.sh
+
+pause
--- .github/release.sh
+++ .github/release.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-TAG=$(git describe --tags --abbrev=0 @^)
-echo "$TAG"
-COMMITS="$(git log "$TAG"..@ --oneline --pretty=format:"%h - %s (%an, %ar)" | tr -s "\n" " ")"
+TAG=$(git tag | sort -V -r | awk 'NR==1')
+PREV_TAG=$(git tag | sort -V -r | awk 'NR==2')
 
-#echo "$COMMITS"
+echo "$TAG"
+echo "$PREV_TAG"
+COMMITS="$(git log "$TAG".."$PREV_TAG" --oneline --pretty=format:"%h - %s (%an, %ar)")" > ticket.txt
+
+echo "$COMMITS"
 
 #curl -H "Authorization: OAuth AQAAAAAc1G31AAd4vp4Ts7KVD0dTnFyQ5N3VliU" \
 #     -H "X-Org-ID: 6461097" \
 #     "https://api.tracker.yandex.net/v2/myself"
 #
 #echo aaa
-curl -H "Content-Type: application/json" \
-     -H "Authorization: OAuth AQAAAAAc1G31AAd4vp4Ts7KVD0dTnFyQ5N3VliU" \
-     -H "X-Org-ID: 6461097" \
-     https://api.tracker.yandex.net/v2/issues/ \
-     -d "{\"queue\": \"TMP\",\"summary\": \"Release $TAG\",\"description\": \"$COMMITS\"}" \
-     > ticket.txt
+#curl -H "Content-Type: application/json" \
+#     -H "Authorization: OAuth AQAAAAAc1G31AAd4vp4Ts7KVD0dTnFyQ5N3VliU" \
+#     -H "X-Org-ID: 6461097" \
+#     https://api.tracker.yandex.net/v2/issues/ \
+#     -d "{\"queue\": \"TMP\",\"summary\": \"Release $TAG\",\"description\": \"$COMMITS\"}" \
+#     > ticket.txt
