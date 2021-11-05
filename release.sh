5a88dea86008154b6ed7dcde295a81f2a795062c
Tufra <60471036+Tufra@users.noreply.github.com>
2021-11-05 15:10:59 +0300

    Update release.sh

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ TAG=$(git describe --tags --abbrev=0 @^)
 echo "$TAG"
 COMMITS="$(git log "$TAG"..@ --oneline --pretty=format:"%h - %s (%an, %ar)" | tr -s "\n" " ")"
 
-#echo "$COMMITS"
+echo "${COMMITS}"
 
 #curl -H "Authorization: OAuth AQAAAAAc1G31AAd4vp4Ts7KVD0dTnFyQ5N3VliU" \
 #     -H "X-Org-ID: 6461097" \
